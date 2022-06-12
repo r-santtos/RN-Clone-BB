519cbde97840f9531221ab6d031d00f6c272ae1e
r-santtos <rsanttos.dev@gmail.com>
2022-06-12 12:52:40 -0300

    finish

--- App.tsx
+++ App.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import Routes from './src/routes';
 
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 export default function App() {
   return (
-    <>
-      <StatusBar style='auto' />
+    <SafeAreaProvider
+      style={{
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#0a61a9',
+      }}
+    >
+      <StatusBar style='light' backgroundColor='rgba(0,0,0, 0.3)' />
       <Routes />
-    </>
+    </SafeAreaProvider>
   );
 }
